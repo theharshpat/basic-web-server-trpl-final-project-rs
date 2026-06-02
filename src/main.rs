c79dbe86d18f2ef76c00a85c274e68e01e10f99c
theharshpat <harsh.patel.magic105@gmail.com>
2026-06-02 08:22:30 +0530

    end of ch 21.3 and ch 21 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use basic_web_server_trpl_final_project_rs::ThreadPool;
 use std::{
     fs,
     io::{BufReader, prelude::*},
@@ -6,26 +7,26 @@ use std::{
     time::Duration,
 };
 
-use basic_web_server_trpl_final_project_rs::ThreadPool;
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = match request_line.as_str() {
+    let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
